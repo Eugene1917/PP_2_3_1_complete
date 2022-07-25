a08c84ac61ab462d7c26319e7255a41519925073
Eugene1917 <Bloody.30@mail.ru>
2022-07-25 14:53:39 +0400

    Initial commit

---
+++ src/main/java/myApp/config/DBconfig.java
@@ -0,0 +1,71 @@
+package myApp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("myApp")
+@PropertySource("classpath:db.properties")
+public class DBconfig {
+
+    private final Environment env;
+
+    @Autowired
+    public DBconfig(Environment env) {
+        this.env = env;
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emFactory = new LocalContainerEntityManagerFactoryBean();
+        emFactory.setDataSource(dataSource());
+        emFactory.setPackagesToScan("myApp");
+        emFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        emFactory.setJpaProperties(properties);
+
+        return emFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+
+}
+
---
+++ src/main/java/myApp/initDb/InitUsersDB.java
@@ -0,0 +1,26 @@
+package myApp.initDb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import myApp.model.User;
+import myApp.service.UserService;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitUsersDB {
+
+    private UserService userService;
+
+    @Autowired
+    public InitUsersDB(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void initApiUserData() {
+        userService.add(new User("Eugene","Eugene@mail.ru",(byte) 22 ));
+        userService.add(new User("Mira","Mira@mail.ru",(byte) 23));
+        userService.add(new User("Lilia","Lili@mail.ru",(byte) 21));
+    }
+}
